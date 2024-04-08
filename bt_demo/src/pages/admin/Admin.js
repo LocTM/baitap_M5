378ae8b813cdff30ed4cba2edc6fc7b21aff3d38
LocTM <tranminhloc.anhk@gmail.com>
2024-04-08 15:57:07 +0700

    main

--- bt_demo/src/pages/admin/Admin.js
+++ bt_demo/src/pages/admin/Admin.js
@@ -1,14 +1,18 @@
 import {Outlet, useNavigate} from "react-router";
 import ListProductAdmin from "./product/ListProductAdmin";
+import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Admin() {
     return (
         <>
             <div className="row">
                 <div className="col-3" style={{border:'1px solid', height: '100vh'}}>
-                    <h2>List Product</h2>
-                    <h2>List Order</h2>
-                    <h2>List User</h2>
+                    <Link to={""}><h2>List Product</h2></Link>
+                    <Link to={"order"}><h2>List Order</h2></Link>
+                    <Link to={"user"}><h2>List User</h2></Link>
+                    <Link to={"/"}><h2>Log Out</h2></Link>
+
                 </div>
                 <div className="col-9" style={{border:'1px solid', height: '100vh'}}>
                     <div className="row">
